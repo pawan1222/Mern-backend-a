e08d6841f671efdc974c7178d2c5ed1cfe0d8056
pawan1222 <pawansingh6388251426@gmail.com>
2025-07-08 10:50:24 +0530

    update

---
+++ server.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import express from "express";
+import dotenv from "dotenv";
+import userRouter from "./routes/userRoute.js";
+dotenv.config();
+
+const app = express();
+const dbuser = encodeURIComponent(process.env.DBUSER);
+const dbpass = encodeURIComponent(process.env.DBPASS);
+
+mongoose
+  // .connect(`mongodb://${dbuser}:${dbpass}@localhost:27017/lpu?authsource=admin`)
+  .connect(`mongodb+srv://${dbuser}:${dbpass}@cluster0.10aivni.mongodb.net/mern-db?retryWrites=true&w=majority&appName=Cluster0`)
+  .then(() => {
+    app.listen(8080, () => {
+      console.log("Server started");
+    });
+  });
+app.use(express.json());
+app.use("/api/users", userRouter);
