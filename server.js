08bfcd2b3f395690b50646a573ba8d3fc9f7decc
pawan1222 <pawansingh6388251426@gmail.com>
2025-07-13 20:36:22 +0530

    update

--- server.js
+++ server.js
@@ -1,26 +1,27 @@
-import mongoose from "mongoose";
 import express from "express";
+import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/userRoute.js";
-import cors from 'cors';
+import cors from "cors"
 dotenv.config();
-
 const app = express();
-<<<<<<< HEAD
-=======
-app.use(cors());
->>>>>>> bb3239c9c51dc675588653bcfe88eee10bfa8ad1
-app.use(express.static("public"));
+app.use(cors())
+app.use(express.json());
 const dbuser = encodeURIComponent(process.env.DBUSER);
 const dbpass = encodeURIComponent(process.env.DBPASS);
 
+// mongoose.connect(`mongodb://localhost:27017/merncafe`).then(() => {
+//   app.listen(8080, () => {
+//     console.log("Server started");
+//   });
+// });
+
 mongoose
-  // .connect(`mongodb://${dbuser}:${dbpass}@localhost:27017/lpu?authsource=admin`)
   .connect(`mongodb+srv://${dbuser}:${dbpass}@cluster0.10aivni.mongodb.net/mern-db?retryWrites=true&w=majority&appName=Cluster0`)
   .then(() => {
     app.listen(8080, () => {
       console.log("Server started");
     });
   });
-app.use(express.json());
+
 app.use("/api/users", userRouter);
