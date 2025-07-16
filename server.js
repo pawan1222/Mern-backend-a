41ee9d753428d447d2a719a419b44983802a5c9f
pawan1222 <pawansingh6388251426@gmail.com>
2025-07-16 11:14:39 +0530

    update

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/userRoute.js";
+import productRouter from "./routes/productRoute.js";
+import orderRouter from "./routes/orderRoute.js"
 import cors from "cors"
 dotenv.config();
 const app = express();
@@ -10,18 +12,20 @@ app.use(express.json());
 const dbuser = encodeURIComponent(process.env.DBUSER);
 const dbpass = encodeURIComponent(process.env.DBPASS);
 
-// mongoose.connect(`mongodb://localhost:27017/lpua`).then(() => {
-//   app.listen(8080, () => {
-//     console.log("Server started");
-//   });
-// });
-
-mongoose
-  .connect(`mongodb+srv://${dbuser}:${dbpass}@cluster0.10aivni.mongodb.net?retryWrites=true&w=majority&appName=Cluster0`)
-  .then(() => {
-    app.listen(8080, () => {
-      console.log("Server started");
-    });
+mongoose.connect(`mongodb://localhost:27017/lpua`).then(() => {
+  app.listen(8080, () => {
+    console.log("Server started");
   });
+});
+
+// mongoose
+//   .connect(`mongodb+srv://${dbuser}:${dbpass}@cluster0.10aivni.mongodb.net?retryWrites=true&w=majority&appName=Cluster0`)
+//   .then(() => {
+//     app.listen(8080, () => {
+//       console.log("Server started");
+//     });
+//   });
 
 app.use("/api/users", userRouter);
+app.use("/api/products", productRouter);
+app.use("/api/orders", orderRouter)
