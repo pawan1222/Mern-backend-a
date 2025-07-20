598fd6e895a3f0b4b12508921c390ef8ac4fbe35
pawan1222 <pawansingh6388251426@gmail.com>
2025-07-20 20:45:35 +0530

    update

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { newOrder,showOrders } from "../controllers/orderController.js";
+import { newOrder,showOrders,showAllOrders,updateOrder } from "../controllers/orderController.js";
 const Router = express.Router();
 
 Router.post("/", newOrder);
 Router.get("/:id", showOrders);
+Router.get("/", showAllOrders);       // e.g. /api/orders?status=Pending&page=1&limit=5
+Router.put("/:id", updateOrder);      // to update order status
+
 
 
 export default Router;
--- server.js
+++ server.js
@@ -12,19 +12,19 @@ app.use(express.json());
 const dbuser = encodeURIComponent(process.env.DBUSER);
 const dbpass = encodeURIComponent(process.env.DBPASS);
 
-mongoose.connect(`mongodb://localhost:27017/lpua`).then(() => {
-  app.listen(8080, () => {
-    console.log("Server started");
-  });
-});
-
-// mongoose
-//   .connect(`mongodb+srv://${dbuser}:${dbpass}@cluster0.10aivni.mongodb.net?retryWrites=true&w=majority&appName=Cluster0`)
-//   .then(() => {
-//     app.listen(8080, () => {
-//       console.log("Server started");
-//     });
+// mongoose.connect(`mongodb://localhost:27017/lpua`).then(() => {
+//   app.listen(8080, () => {
+//     console.log("Server started");
 //   });
+// });
+
+mongoose
+  .connect(`mongodb+srv://${dbuser}:${dbpass}@cluster0.10aivni.mongodb.net/mern-db?retryWrites=true&w=majority&appName=Cluster0`)
+  .then(() => {
+    app.listen(8080, () => {
+      console.log("Server started");
+    });
+  });
 
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
